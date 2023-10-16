4488c39fc237372ecf8827881ab2d8c05469fbe5
IMTR0J4N <113339445+IMTR0J4N@users.noreply.github.com>
2023-10-16 17:16:24 +0200

    .

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,29 +1,45 @@
 import { auth } from "../../services/firebase";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
-import GoogleSignin from "../../assets/btn_google_signin_dark_pressed_web.png";
+import GoogleSignin from "../../assets/google.jpg";
+import GithubSignin from "../../assets/github.png"
 import { Navigate } from "react-router-dom";
 import './Login.css'
 
 function Login() {
 
-    const [user] = useAuthState(auth)
+    const [user, loading] = useAuthState(auth)
 
-    const googleSignIn = () => {
-        const provider = new GoogleAuthProvider();
-        signInWithRedirect(auth, provider)
+    const SignIn = (provider) => {
+        signInWithRedirect(auth, provider);
     };
 
     if(user) {
+        console.log(user, "user")
         return <Navigate replace to="/"/>
+    } else if (loading) {
+        console.log(user, "loading")
+        return (
+            <>
+            <h2>Loading User, please wait...</h2>
+            <div className="container">
+                <div className="dash uno"></div>
+                <div className="dash dos"></div>
+                <div className="dash tres"></div>
+                <div className="dash cuatro"></div>
+            </div>
+            </>
+        )
     } else {
         return (
             <div className="login-wrapper">
                 <div className="login-container">
                     <h1>Sign UP</h1>
-                    <button className="sign-in">
-                        <img src={GoogleSignin} onClick={googleSignIn} /> 
-                    </button>
+                    <div className="sign-in-container">
+                        <button className="google-sign-in">
+                            <img src={GoogleSignin} onClick={() => (SignIn(new GoogleAuthProvider()))} /> 
+                        </button>
+                    </div>
                 </div> 
             </div>
         )
